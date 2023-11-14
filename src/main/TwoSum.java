11ee772f980d1f5845fef550e42a77767b7d4f9d
chase-michael <chase.zelechowski@gmail.com>
2023-11-14 17:49:35 -0500

    fixed return statement

--- src/main/TwoSum.java
+++ src/main/TwoSum.java
@@ -5,16 +5,13 @@ import java.util.Arrays;
 public class TwoSum {
 
     public static int[] twoSum(int[] nums, int target) {
-        int[] targetElements = new int[2];
 
         for (int i = 0; i < nums.length; i++) {
             int neededValue = target - nums[i];
 
             for (int j = i + 1; j < nums.length; j++) {
                 if (nums[j] == neededValue) {
-                    targetElements[0] = i;
-                    targetElements[1] = j;
-                    return targetElements;
+                    return new int[] {i, j};
                 }
             }
         }
